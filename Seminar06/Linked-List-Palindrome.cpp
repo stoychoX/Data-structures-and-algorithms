ac43d43d4b9a67b47b02648fc1bb0153191673e9
Stoycho Kyosev <68247567+stoychoX@users.noreply.github.com>
2022-11-10 00:46:45 +0200

    Update and rename Linked-List-Perm.cpp to Linked-List-Palindrome.cpp

--- Seminar06/Linked-List-Palindrome.cpp
+++ Seminar06/Linked-List-Palindrome.cpp
@@ -1,28 +1,28 @@
 #include"../Seminar04/linked-list-utils.h"
 
-bool isLinkedListPermRec(const LinkedListNode<int>*& begin, const LinkedListNode<int>* end) {
+bool isLinkedListPalRec(const LinkedListNode<int>*& begin, const LinkedListNode<int>* end) {
     if(end == nullptr)
         return true;
 
-    bool res = isLinkedListPermRec(begin, end->next) && (begin->data == end->data);
+    bool res = isLinkedListPalRec(begin, end->next) && (begin->data == end->data);
 
     begin = begin->next;
     return res;
 }
 
 template<class T>
-bool isLinkedListPerm(const LinkedListNode<T>* ll) {
+bool isLinkedListPal(const LinkedListNode<T>* ll) {
     if(ll == nullptr)
         return true;
     
     const LinkedListNode<int>* begin = ll;
     const LinkedListNode<int>* end = ll;
     
-    return isLinkedListPermRec(begin, end);
+    return isLinkedListPalRec(begin, end);
 }
 
 int main() {
     LinkedListNode<int>* ll = new LinkedListNode(0, new LinkedListNode(1, new LinkedListNode(1)));
 
-    std::cout << isLinkedListPerm(ll);
+    std::cout << isLinkedListPal(ll);
 }
