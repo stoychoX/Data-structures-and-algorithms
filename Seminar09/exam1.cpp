c1b3267208163f17c08d545f805f0b412d35d377
Stoycho Kyosev <stoycho.kk@gmail.com>
2022-12-21 15:11:33 +0200

    fix leak

--- Seminar09/exam1.cpp
+++ Seminar09/exam1.cpp
@@ -31,24 +31,5 @@ int findSum(const Tree<int>* t, int x, int y) {
 }
 
 int main() {
-    Tree<int>* root = new Tree<int>(50);
-	Tree<int>* n1 = new Tree<int>(25);
-	Tree<int>* n2 = new Tree<int>(75);
-	Tree<int>* n3 = new Tree<int>(12);
-	Tree<int>* n4 = new Tree<int>(30);
-	Tree<int>* n5 = new Tree<int>(60);
-	Tree<int>* n6 = new Tree<int>(85);
-	Tree<int>* n7 = new Tree<int>(52);
-	Tree<int>* n8 = new Tree<int>(70);
-
-	root->left = n1;
-	root->right = n2;
-	n1->left = n3;
-	n1->right = n4;
-	n2->left = n5;
-	n2->right = n6;
-	n5->left = n7;
-	n5->right = n8;
-
-	std::cout << findSum(root, 30, 75);
+    
 }
